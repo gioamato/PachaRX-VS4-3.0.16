9234a05b1f715740384b85d276c9260fd3656382
Ing. Vincenzo Amato <jonnynapoli91@hotmail.it>
2013-06-29 11:19:54 +0200

    broken git again XD recommit...

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 52 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#52 PREEMPT Sat Jun 29 11:05:32 CEST 2013"
+#define LINUX_COMPILE_BY "zippy"
+#define LINUX_COMPILE_HOST "pc-vincenzo"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.0.16-PachaRX-VS4-2.9_release--ge37e67f-dirty"
